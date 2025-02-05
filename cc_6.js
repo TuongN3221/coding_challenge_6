48be347b30262b9e9b73bd41556f8d356a803a98
Tuong Nguyen <yolotube21@yahoo.com>
2025-02-05 10:36:15 -0500

    Task 2 - Sales Tax Computation

--- cc_6.js
+++ cc_6.js
@@ -7,3 +7,13 @@ function calculateProfit(costPrice, sellingPrice, unitsSold) {
 
 console.log(calculateProfit(20, 30, 100)); // Expected output: "Total Profit: $1000"
 console.log(calculateProfit(50, 70, 200)); // Expected output: "Total Profit: $4000"
+
+// Task 2 - Sales Tax Computation
+function calculateSalesTax(amount, taxRate) {
+    const salesTax = amount * taxRate
+
+    return `Sales Tax: ${salesTax}`
+}
+
+console.log(calculateSalesTax(100, 0.07)); // Expected output: "Sales Tax: $7"
+console.log(calculateSalesTax(500, 0.1));  // Expected output: "Sales Tax: $50"
