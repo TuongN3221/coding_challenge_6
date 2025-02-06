57726bb14de431c7f0eacb7c54e32dea599e18b2
Tuong Nguyen <yolotube21@yahoo.com>
2025-02-05 21:50:59 -0500

    Task 3 - Employee Bonus Calculation

--- cc_6.js
+++ cc_6.js
@@ -17,3 +17,19 @@ function calculateSalesTax(amount, taxRate) {
 
 console.log(calculateSalesTax(100, 0.07)); // Expected output: "Sales Tax: $7"
 console.log(calculateSalesTax(500, 0.1));  // Expected output: "Sales Tax: $50"
+
+// Task 3 - Employee Bonus Calculation
+const calculateBonus = (salary, performanceRating) => {
+    const bonusPercent = {
+        Excellent: 0.2,
+        Good: 0.1,
+        Average: 0.05
+    };
+    const bonus = salary * bonusPercent[performanceRating];
+    return `Bonus: $${bonus.toFixed(2)}`;
+}
+console.log(calculateBonus(5000, "Excellent")); // Expected output: "Bonus: $1000"
+console.log(calculateBonus(7000, "Good"));      // Expected output: "Bonus: $700"
+
+
+
